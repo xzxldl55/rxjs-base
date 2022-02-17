0965a02cfd4149a939141dd107fac8de4ebd2c06
Taoist <476630095@qq.com>
2022-02-17 23:21:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*') //添加跨域！！！！不添加会报错
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'text/palin; charset=utf-8') //防止乱码
   console.log(req.url)
   if (req.url === '/test')
